c9aa03e16f10ee73fe5eefe2ef610e87ed3aa1a8
yuliiasobol <yuliia.sobol@globallogic.com>
2015-10-02 17:10:16 +0300

    KnownType investigation
    KnownType investigation

--- Store/StoreServices/DTOLibraries/UserDTO/User.cs
+++ Store/StoreServices/DTOLibraries/UserDTO/User.cs
@@ -3,11 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Runtime.Serialization;
 
 namespace DTOLibraries.UserDTO
 {
+	[KnownType(typeof(RegisteredUser))]//указываем что RegisteredUser тоже нужно сериализовать 
+	[DataContract]
 	public class User
 	{
+		[DataMember(Order = 2)] //по умолчанию идет по алфавиту
 		public string Name { get; set; }
 		public string Email { get; set; }
 		public Guid Id { get; set; }

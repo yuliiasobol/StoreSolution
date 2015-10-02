f96ff057834bb099e6e55649824be30f74bdd443
yuliiasobol <yuliia.sobol@globallogic.com>
2015-10-02 16:12:48 +0300

    added web application
    added web application

---
+++ StoreWebApplication/StoreWebApplication/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="StoreWebApplication.MvcApplication" Language="C#" %>
---
+++ StoreWebApplication/StoreWebApplication/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(StoreWebApplication.Startup))]
+namespace StoreWebApplication
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

5f2ff786007b50bdc2141ce4d8ec6e7b478f2459
yuliiasobol <yuliia.sobol@globallogic.com>
2015-10-02 16:02:50 +0300

    other changes
    other changes

--- Store/StoreServices/ServiceHost/Program.cs
+++ Store/StoreServices/ServiceHost/Program.cs
@@ -6,13 +6,20 @@ using System.Threading.Tasks;
 using System.ServiceModel;
 using System.ServiceModel.Description;
 
+
 namespace ServiceHost
 {
 	class Program
 	{
 		static void Main(string[] args)
 		{
-		
+			using (var host = new System.ServiceModel.ServiceHost(typeof (StoreServicesLibrary.UserService)))
+			{
+				host.Open();
+				Console.WriteLine("Server started...");
+				Console.ReadLine();
+			}
+
 		}
 	}
 }
